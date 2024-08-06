c327c5629e397c45d24a7487c5bb6f437a7e5626
Hamam22 <117502176+Hamam22@users.noreply.github.com>
2024-08-07 04:36:38 +0700

    Update Soundcloud.py

--- YukkiMusic/platforms/Soundcloud.py
+++ YukkiMusic/platforms/Soundcloud.py
@@ -1,23 +1,15 @@
-#
-# Copyright (C) 2021-2022 by TeamYukki@Github, < https://github.com/TeamYukki >.
-#
-# This file is part of < https://github.com/TeamYukki/YukkiMusicBot > project,
-# and is released under the "GNU v3.0 License Agreement".
-# Please see < https://github.com/TeamYukki/YukkiMusicBot/blob/master/LICENSE >
-#
-# All rights reserved.
-
 import re
 from os import path
-
 from yt_dlp import YoutubeDL
-
-from YukkiMusic.utils.formatters import seconds_to_min
+from AnonXMusic.utils.formatters import seconds_to_min
 
 
 class SoundAPI:
     def __init__(self):
         self.opts = {
+            "cookiefile":
+"cookies.txt",
+            "verbose": True,
             "outtmpl": "downloads/%(id)s.%(ext)s",
             "format": "best",
             "retries": 3,
